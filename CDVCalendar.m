080c00f9f36c779b5d18466fc0aabfd0d1faded4
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-01-12 01:09:38 -0500

    init

---
+++ CDVCalendar.m
@@ -0,0 +1,103 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ */
+//
+//  CDVCalendar
+//  CDVEmailComposer Template Created Jan 7 2013
+//  Copyright 2013 @RandyMcMillan
+
+#import <Cordova/CDVAvailability.h>
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVDebug.h>
+
+#import "CDVCalendar.h"
+#import "CDVCalendar_JS.h"
+
+@implementation CDVCalendar
+
+- (void)init:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+{
+	NSLog(@"The CDVCalendar init function was called!");
+
+	NSString *callbackId = [arguments pop];
+	// NSString *objectAtIndex0 = [arguments objectAtIndex:0];
+
+	CDVViewController	*mvcCDVCalendar = (CDVViewController *)[super viewController];
+	NSString			*jsString = kCDVCalendarINIT;
+
+	[mvcCDVCalendar.webView stringByEvaluatingJavaScriptFromString:jsString];
+
+	NSString		*resultType = [arguments objectAtIndex:0];
+	CDVPluginResult *result;
+
+	if ([resultType isEqualToString:@"success"]) {
+		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Success! const kCDVCalendarALERT was evaluated by webview and created alert!"];
+
+		NSLog(@"callbackId = '%@'", callbackId);
+		[self writeJavascript:[result toSuccessCallbackString:callbackId]];
+	} else {
+		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"resultType = 'error'! const kCDVCalendarALERT was evaluated by webview and created alert!"];
+
+		NSLog(@"callbackId = '%@'", callbackId);
+		[self writeJavascript:[result toErrorCallbackString:callbackId]];
+	}
+}
+
+- (void)nativeFunction:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+{
+	NSLog(@"Hello, this is a native function called from CDVCalendar!");
+
+	// get the callback id
+	NSString	*callbackId		= [arguments pop];
+	NSString	*objectAtIndex0 = [arguments objectAtIndex:0];
+	NSLog(@"objectAtIndex0 = '%@'", objectAtIndex0);
+
+	/*
+	 *        NSString *objectAtIndex1 = [arguments objectAtIndex:1];
+	 *        NSLog(@"objectAtIndex1 = '%@'",objectAtIndex1);
+	 */
+	NSLog(@"kCDVCalendarALERT = %@", kCDVCalendarALERT);
+
+	CDVViewController *mvcCDVCalendar = (CDVViewController *)[super viewController];
+	NSLog(@"mvcCDVCalendar = %@", mvcCDVCalendar);
+	NSLog(@"mvcCDVCalendar.view = %@", mvcCDVCalendar.view);
+	NSLog(@"mvcCDVCalendar.webView = %@", mvcCDVCalendar.webView);
+
+	//    mvcCDVCalendar.webView.alpha = 0.5;
+
+	NSString *jsString = kCDVCalendarALERT;
+	[mvcCDVCalendar.webView stringByEvaluatingJavaScriptFromString:jsString];
+
+	NSString *resultType = [arguments objectAtIndex:0];
+	NSLog(@"%@", resultType);
+	CDVPluginResult *result;
+
+	if ([resultType isEqualToString:@"success"]) {
+		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Success! const kCDVCalendarALERT was evaluated by webview and created alert!"];
+
+		NSLog(@"callbackId = '%@'", callbackId);
+		[self writeJavascript:[result toSuccessCallbackString:callbackId]];
+	} else {
+		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"resultType = 'error'! const kCDVCalendarALERT was evaluated by webview and created alert!"];
+
+		NSLog(@"callbackId = '%@'", callbackId);
+		[self writeJavascript:[result toErrorCallbackString:callbackId]];
+	}
+}
+
+@end
